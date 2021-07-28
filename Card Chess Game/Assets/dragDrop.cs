e4c15beee1dca123381c0ff33fc398311ed6365c
ddooooo <shb05377@gmail.com>
2021-07-28 22:39:28 +0900

    0728

--- Card Chess Game/Assets/dragDrop.cs
+++ Card Chess Game/Assets/dragDrop.cs
@@ -1,21 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.EventSystems;
+using UnityEditor;
+
 public class dragDrop : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
 {
+    // To find the mouse position in UI
     GameObject canvas;
     GameObject hands;
     public static bool beingHeld;
-    public GameObject horsePiece1;
-    public GameObject horsePiece2;
-    GameObject indicator;
+    public GameObject pieces;
+    public static string pieceName;
     void Start() {
         canvas = GameObject.Find("Canvas");
         hands = GameObject.Find("Hands");
-        indicator = GameObject.Find("selectedIndicator");
-        horsePiece1 = GameObject.Find("test piece");
-        horsePiece2 = GameObject.Find("test piece_2");
+        pieces = GameObject.Find("Chess Piece");
     }
     public void OnBeginDrag(PointerEventData eventData) {
         Debug.Log("OnBeginDrag");
@@ -26,26 +27,78 @@ public class dragDrop : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDrag
     public void OnDrag(PointerEventData eventData) {
         this.transform.position = eventData.position;
     }
-
+    public static bool moved = false;
     public void OnEndDrag(PointerEventData eventData) {
         Debug.Log("OnEndDrag");
-        if(horsePiece1.GetComponent<CircleCollider2D>().IsTouching(gameObject.GetComponent<BoxCollider2D>())) {
-            //when card is dropped to horse piece
-            Debug.Log("Card will be destroyed and horsepiece1 will move");
-            indicator.transform.position = horsePiece1.transform.position;
-            indicator.transform.SetParent(horsePiece1.transform);
-            horsePiece1.transform.position = new Vector2(horsePiece1.transform.position.x, horsePiece1.transform.position.y+160);
-            Destroy(gameObject);
-        }else if(horsePiece2.GetComponent<CircleCollider2D>().IsTouching(gameObject.GetComponent<BoxCollider2D>())) {
-            Debug.Log("Card will be destroyed and horsepiece2 will move");
-            indicator.transform.position = horsePiece2.transform.position;
-            indicator.transform.SetParent(horsePiece2.transform);
-            Destroy(gameObject);
+        foreach(Transform child in pieces.transform) {
+            if(child.GetComponent<CircleCollider2D>().IsTouching(gameObject.GetComponent<BoxCollider2D>())) {
+                Debug.Log(child.gameObject.name + " will be moved");
+                pieceName = child.gameObject.name;
+                moved = true;
+                Destroy(gameObject);
+            }
         }
+        
+            //when card is dropped to horse piece
 
+            /*indicator.transform.position = horsePiece1.transform.position;
+            indicator.transform.SetParent(horsePiece1.transform);
+            //horsePiece1.transform.position = new Vector2(horsePiece1.transform.position.x, horsePiece1.transform.position.y+160);
+
+            // in case of moving archer
+            Object prefab = AssetDatabase.LoadAssetAtPath("Assets/Prefab/dot.prefab", typeof(GameObject));
+            GameObject dot = Instantiate(prefab) as GameObject;
+            dot.transform.SetParent(moveIndicator.transform, false);
+            GameObject dot_2 = Instantiate(prefab) as GameObject;
+            dot_2.transform.SetParent(moveIndicator.transform, false);
+            dot.transform.position = new Vector2(horsePiece1.transform.position.x+160, horsePiece1.transform.position.y);
+            dot_2.transform.position = new Vector2(horsePiece1.transform.position.x, horsePiece1.transform.position.y+160);*/
+
+        //else if(horsePiece2.GetComponent<CircleCollider2D>().IsTouching(gameObject.GetComponent<BoxCollider2D>())) {
+        //     Debug.Log("Card will be destroyed and horsepiece2 will move");
+        //     indicator.transform.position = horsePiece2.transform.position;
+        //     indicator.transform.SetParent(horsePiece2.transform);
+        //     Destroy(gameObject);
+        //}
         transform.SetParent(hands.transform);
         beingHeld = false;
     }
+
+    /*public void Update() {
+        clickToMove();
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        Debug.Log("Clicked: " + eventData.pointerCurrentRaycast.gameObject.name);
+    }
+
+    public void clickToMove() {
+        if (Input.GetKey(KeyCode.Mouse0)) {
+            //Set up the new Pointer Event
+            var ped = new PointerEventData(null);
+            //Set the Pointer Event Position to that of the mouse position
+            ped.position = Input.mousePosition;
+
+            //Create a list of Raycast Results
+            List<RaycastResult> results = new List<RaycastResult>();
+            EventSystem.current.RaycastAll(ped, results);
+            
+            //For every result returned, output the name of the GameObject on the Canvas hit by the Ray
+            foreach (RaycastResult result in results)
+            {
+                Debug.Log(result.gameObject.name);
+                if(result.gameObject.name == "dot(Clone)") {
+                    horsePiece1.transform.position = result.gameObject.transform.position;
+                    indicator.transform.position = new Vector2(-1000, 1000);
+                    
+                    foreach(Transform child in moveIndicator.transform) {
+                        Destroy(child.gameObject);
+                    }                    
+                }
+            }
+        }
+    }*/
 }
 
 // create selected indicator -->
--- Card Chess Game/Assets/horseController.cs
+++ Card Chess Game/Assets/horseController.cs
@@ -2,29 +2,42 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using UnityEditor;
+
 public class horseController : MonoBehaviour
 {
+    private GraphicRaycaster gr;
+
+    GameObject canvas;
+
     GameObject circle2;
     GameObject circle3;
     GameObject archer1;
     GameObject cardTest;
-    GameObject selectedObject;
-
+    GameObject indicator;
+    GameObject moveIndicator;
+    GameObject child;
     void Start()
     {   
+        canvas = GameObject.Find("Canvas");
         circle2 = GameObject.Find("Circle Test_2");
         circle3 = GameObject.Find("Circle Test_3");
         cardTest = GameObject.Find("cardTest(Clone)");
+        moveIndicator = GameObject.Find("Move Indicator");
+
+        gr = GetComponent<GraphicRaycaster>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        clickToMove();
+
         if(dragDrop.beingHeld) {
             // circle2.GetComponent<SpriteRenderer>().sortingOrder = 2;
             // circle3.GetComponent<SpriteRenderer>().sortingOrder = 2;
             gameObject.GetComponent<Image>().color = Color.blue;
-
             // if(gameObject.GetComponent<CircleCollider2D>().IsTouching(cardTest.GetComponent<BoxCollider2D>())) {
             // //when card is dropped to horse piece
             //     Debug.Log("Selected This Piece!");
@@ -34,6 +47,58 @@ public class horseController : MonoBehaviour
             // circle2.GetComponent<SpriteRenderer>().sortingOrder = 0;
             // circle3.GetComponent<SpriteRenderer>().sortingOrder = 0;
             gameObject.GetComponent<Image>().color = Color.white;
+
+            if(dragDrop.moved) {
+                child = GameObject.Find(dragDrop.pieceName);
+                Object selected = AssetDatabase.LoadAssetAtPath("Assets/Prefab/selectedIndicator.prefab", typeof(GameObject));
+                if(selected == null) {
+                    Debug.Log("it is null");
+                }
+                Debug.Log(child.name);
+                indicator = Instantiate(selected) as GameObject;
+                indicator.transform.SetParent(canvas.transform);
+                indicator.transform.position = child.transform.position;
+                // indicator.transform.SetParent(gameObject.transform);
+                //horsePiece1.transform.position = new Vector2(horsePiece1.transform.position.x, horsePiece1.transform.position.y+160);
+
+                // in case of moving archer
+                Object prefab = AssetDatabase.LoadAssetAtPath("Assets/Prefab/dot.prefab", typeof(GameObject));
+
+                GameObject dot = Instantiate(prefab) as GameObject;
+                dot.transform.SetParent(moveIndicator.transform, false);
+                GameObject dot_2 = Instantiate(prefab) as GameObject;
+                dot_2.transform.SetParent(moveIndicator.transform, false);
+                dot.transform.position = new Vector2(child.transform.position.x+160, child.transform.position.y);
+                dot_2.transform.position = new Vector2(child.transform.position.x, child.transform.position.y+160);
+                dragDrop.moved = false;
+            }
+        }
+    }
+
+    public void clickToMove() {
+        if (Input.GetKey(KeyCode.Mouse0)) {
+            //Set up the new Pointer Event
+            var ped = new PointerEventData(null);
+            //Set the Pointer Event Position to that of the mouse position
+            ped.position = Input.mousePosition;
+
+            //Create a list of Raycast Results
+            List<RaycastResult> results = new List<RaycastResult>();
+            EventSystem.current.RaycastAll(ped, results);
+            
+            //For every result returned, output the name of the GameObject on the Canvas hit by the Ray
+            foreach (RaycastResult result in results)
+            {
+                //Debug.Log(result.gameObject.name);
+                if(result.gameObject.name == "dot(Clone)") {
+                    child.transform.position = result.gameObject.transform.position;
+                    Destroy(indicator);
+                    
+                    foreach(Transform child in moveIndicator.transform) {
+                        Destroy(child.gameObject);
+                    }                    
+                }
+            }
         }
     }
 }
